a26931fb3f3ec5923f5558c09df7bcf8f6d3f1f7
eliang <eliang.cs@gmail.com>
2013-09-03 01:13:02 +0800

    Extract 10-Q form links.

--- stockcrawler/spiders/edgar.py
+++ stockcrawler/spiders/edgar.py
@@ -1,25 +1,37 @@
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
-from scrapy.spider import BaseSpider
 
 
 class URLGenerator(object):
 
     def __iter__(self):
-        symbols = ('GOOG', 'APPL', 'FB')
+        symbols = ('FB',)
         url = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=%s&type=10-Q&dateb=&owner=exclude&count=100'
 
         for symbol in symbols:
             yield (url % symbol)
 
+# http://www.sec.gov/Archives/edgar/data/1326801/000132680113000019/0001326801-13-000019-index.htm
+# http://www.sec.gov/Archives/edgar/data/1326801/000132680113000019/fb-6302013x10q.htm
+# http://www.sec.gov/Archives/edgar/data/1288776/000119312509101727/d10q.htm
+# http://www.sec.gov/Archives/edgar/data/1288776/000119312504142809/0001193125-04-142809-index.htm
+# http://www.sec.gov/Archives/edgar/data/1288776/000119312504141838/0001193125-04-141838-index.htm
+# http://www.sec.gov/Archives/edgar/data/1288776/000119312504142809/d10qa.htm
 
-class EdgarSpider(BaseSpider):
+
+class EdgarSpider(CrawlSpider):
 
     name = 'edgar'
     allowed_domains = ['sec.gov']
     start_urls = URLGenerator()
 
-    def parse(self, response):
+    rules = (
+        Rule(SgmlLinkExtractor(allow=('/Archives/edgar/data/[^\"]+\-index\.htm',))),
+        Rule(SgmlLinkExtractor(allow=('/Archives/edgar/data/[^\"]+10qa?\.htm',)), callback='parse_10q'),
+    )
+
+    def parse_10q(self, response):
         hxs = HtmlXPathSelector(response)
-        doc_urls = hxs.select('//a[starts-with(@href, "/Archives/edgar/data/")]/@href').extract()
-        for url in doc_urls:
-            print 'URL: %s' % url
+        print '--------------------------------------------'
+        print hxs.select('//title/text()').extract()
