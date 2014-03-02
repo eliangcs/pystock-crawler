245ecb7139dec37368ffc975f69b3daaf2582460
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-02 14:16:53 +0800

    Make setup.py more robust.

--- setup.py
+++ setup.py
@@ -3,6 +3,35 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import codecs
+import os
+import re
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string')
+
+
+def read_description(filename):
+    with codecs.open(filename, encoding='utf-8') as f:
+        return f.read()
+
 
 def parse_requirements(filename):
     with open(filename) as f:
@@ -12,10 +41,10 @@ def parse_requirements(filename):
 
 setup(
     name='pystock-crawler',
-    version='0.0.1',
+    version=find_version('pystock_crawler', '__init__.py'),
     url='https://github.com/eliangcs/pystock-crawler',
     description='Crawl stock historical data.',
-    long_description=open('README.rst').read(),
+    long_description=read_description('README.rst'),
     author='Chang-Hung Liang',
     author_email='eliang.cs@gmail.com',
     license='MIT',
