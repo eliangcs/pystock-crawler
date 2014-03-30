3edafa72f0cf2a1541945a5f14644522be145811
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-30 20:44:31 +0800

    Add some fields that are going to be tested

--- pystock_crawler/tests/base.py
+++ pystock_crawler/tests/base.py
@@ -24,13 +24,19 @@ class TestCaseBase(unittest.TestCase):
         self.assertEqual(item.get('period_focus'), expected.get('period_focus'))
         self.assertEqual(item.get('end_date'), expected.get('end_date'))
         self.assert_none_or_almost_equal(item.get('revenues'), expected.get('revenues'))
+        self.assert_none_or_almost_equal(item.get('op_income'), expected.get('op_income'))
         self.assert_none_or_almost_equal(item.get('net_income'), expected.get('net_income'))
         self.assert_none_or_almost_equal(item.get('eps_basic'), expected.get('eps_basic'))
         self.assert_none_or_almost_equal(item.get('eps_diluted'), expected.get('eps_diluted'))
         self.assertAlmostEqual(item.get('dividend'), expected.get('dividend'))
         self.assert_none_or_almost_equal(item.get('assets'), expected.get('assets'))
+        self.assert_none_or_almost_equal(item.get('cur_assets'), expected.get('cur_assets'))
+        self.assert_none_or_almost_equal(item.get('cur_liab'), expected.get('cur_liab'))
         self.assert_none_or_almost_equal(item.get('equity'), expected.get('equity'))
         self.assert_none_or_almost_equal(item.get('cash'), expected.get('cash'))
+        self.assert_none_or_almost_equal(item.get('cash_flow_op'), expected.get('cash_flow_op'))
+        self.assert_none_or_almost_equal(item.get('cash_flow_inv'), expected.get('cash_flow_inv'))
+        self.assert_none_or_almost_equal(item.get('cash_flow_fin'), expected.get('cash_flow_fin'))
 
 
 def _create_sample_data_dir():
