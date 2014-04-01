934745162cc53e2e14d4cd736e2837848c5c85f9
Chang-Hung Liang <eliang.cs@gmail.com>
2014-04-01 22:30:04 +0800

    Test AXP 2013 Q1

--- pystock_crawler/tests/test_loaders.py
+++ pystock_crawler/tests/test_loaders.py
@@ -416,14 +416,20 @@ class ReportItemLoaderTest(TestCaseBase):
             'doc_type': '10-Q',
             'period_focus': 'Q1',
             'end_date': '2013-03-31',
-            'revenues': 7384000000.0,
-            'net_income': 1280000000.0,
+            'revenues': 7384000000,
+            'op_income': None,
+            'net_income': 1280000000,
             'eps_basic': 1.15,
             'eps_diluted': 1.15,
             'dividend': 0.2,
-            'assets': 156855000000.0,
-            'equity': 19290000000.0,
-            'cash': 27964000000.0
+            'assets': 156855000000,
+            'cur_assets': None,
+            'cur_liab': None,
+            'equity': 19290000000,
+            'cash': 27964000000,
+            'cash_flow_op': 7547000000,
+            'cash_flow_inv': 32000000,
+            'cash_flow_fin': -1830000000
         })
 
     def test_ba_20091231(self):
