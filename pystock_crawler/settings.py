b2d4483f775b2ecca58f6cb0648dc9fa99cc660a
Chang-Hung Liang <eliang.cs@gmail.com>
2014-05-20 22:21:11 +0800

    Tweak settings

--- pystock_crawler/settings.py
+++ pystock_crawler/settings.py
@@ -38,6 +38,10 @@ SPIDER_MODULES = ['pystock_crawler.spiders']
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'pystock-crawler (+http://www.yourdomain.com)'
 
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+
 COOKIES_ENABLED = False
 
 AUTOTHROTTLE_ENABLED = True
+
+RETRY_TIMES = 4
