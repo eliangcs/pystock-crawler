625a28046ed3838db6a0048d12d4836241e5047b
Chang-Hung Liang <eliang.cs@gmail.com>
2014-06-28 22:27:45 +0800

    Fix travis build error

--- pystock_crawler/tests/test_cmdline.py
+++ pystock_crawler/tests/test_cmdline.py
@@ -1,11 +1,10 @@
 import os
 import shutil
-import subprocess
 import unittest
 
 import pystock_crawler
 
-from envoy import run, expand_args
+from envoy import run
 
 
 TEST_DIR = './test_data'
@@ -52,38 +51,6 @@ class CrawlSymbolsTest(unittest.TestCase):
 
     def get_output_content(self):
         output_path = self.args['output']
-
-        os.system('touch %s' % os.path.join(TEST_DIR, 'hello'))
-        cmd = 'scrapy crawl nasdaq -a exchanges="NYSE" -t symbollist -o "/home/travis/build/eliangcs/pystock-crawler/test_data/envoy.txt" -s LOG_FILE="/home/travis/build/eliangcs/pystock-crawler/test_data/envoy.log"'
-        run(cmd)
-        process = subprocess.Popen([
-            'scrapy', 'crawl', 'nasdaq',
-            '-a', 'exchanges="NYSE"',
-            '-t', 'symbollist',
-            '-o', '/home/travis/build/eliangcs/pystock-crawler/test_data/popen.txt',
-            '-s', 'LOG_FILE="/home/travis/build/eliangcs/pystock-crawler/test_data/popen.log"'
-        ], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
-        process.communicate()
-
-        print '### Envoy'
-        with open('./test_data/envoy.txt') as f:
-            print f.read()[0:200]
-
-        print '### Popen'
-        with open('./test_data/popen.txt') as f:
-            print f.read()[0:200]
-
-        print '### expand_args'
-        print expand_args(cmd)
-
-        with open(self.args['log_file']) as f:
-            print f.read()
-
-        print os.getcwd()
-        print os.listdir(os.getcwd())
-        print output_path
-        print os.listdir(TEST_DIR)
-
         self.assertTrue(os.path.isfile(output_path))
 
         with open(output_path) as f:
@@ -125,8 +92,6 @@ class CrawlSymbolsTest(unittest.TestCase):
     def test_crawl_nyse(self):
         r = run('./bin/pystock-crawler symbols NYSE -o %(output)s -l %(log_file)s -w %(working_dir)s' % self.args)
         self.assertEqual(r.status_code, 0)
-        print r.std_out
-        print r.std_err
         self.assert_nyse_output()
         self.assert_log()
         self.assert_cache()
@@ -134,8 +99,6 @@ class CrawlSymbolsTest(unittest.TestCase):
     def test_crawl_nyse_and_nasdaq(self):
         r = run('./bin/pystock-crawler symbols NYSE,NASDAQ -o %(output)s -l %(log_file)s -w %(working_dir)s' % self.args)
         self.assertEqual(r.status_code, 0)
-        print r.std_out
-        print r.std_err
         self.assert_nyse_and_nasdaq_output()
         self.assert_log()
         self.assert_cache()
